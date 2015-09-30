e0539658a953037a4284196eeac478128e6926b9
Liang <zhoul@mit.edu>
2015-09-30 00:27:33 -0400

    sse

---
+++ matlab/findSSE.m
@@ -0,0 +1,11 @@
+function [ SSE, derivative ] = findSSE( X, Y, basis, theta )
+%finds SSE and derivative
+%   Detailed explanation goes here
+
+sum = 0;
+for n = 1:length(X)
+    sum = sum + (Y(n, 1) - theta' * basis(X)).^2; %some things here to add to the sum
+end
+
+end
+
