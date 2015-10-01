2e00fa1fb0f1ff2d20cfa3037c2836be468e3b96
Liang <zhoul@mit.edu>
2015-10-01 17:42:09 -0400

    sections

--- matlab/calcTheta.m
+++ matlab/calcTheta.m
@@ -2,7 +2,7 @@ function [theta] = calcTheta(lambda, X, Y)
 
 err = @(theta)sum(abs(X*theta - Y)) + lambda*sum((theta).^2);
 
-options = optimoptions(@fminunc,'Display','iter','Algorithm','quasi-newton','MaxFunEvals',2800);
+options = optimoptions(@fminunc,'Display','iter','Algorithm','quasi-newton','MaxFunEvals',28000);
 
-theta = fminunc(err, ones(280,1), options);
+theta = fminunc(err, zeros(280,1), options);
 end
