b2e3aa5572563d60bc536d006c8f3539c290feb0
Liang <zhoul@mit.edu>
2015-10-01 22:33:24 -0400

    stuff

--- matlab/gradsseD.m
+++ matlab/gradsseD.m
@@ -5,10 +5,10 @@ grad = zeros(length(theta), 1);
 for i = 1:length(theta)
     theta1 = theta;
     theta2 = theta;
-    theta1(1, i) = theta1(1, i) + 0.00001;
-    theta2(1, i) = theta2(1, i) - 0.00001;
+    theta1(i) = theta1(i) + 0.00001;
+    theta2(i) = theta2(i) - 0.00001;
     for n = 1:length(X)
-        grad(i, 1) = grad(i, 1) + ((Y(n, 1) - theta1 * phii(M, X(n, 1))).^2 - (Y(n, 1) - theta2 * phii(M, X(n, 1))).^2)/0.00001;
+        grad(i, 1) = grad(i, 1) + ((Y(n, 1) - theta1 * phii(M, X(n, 1))).^2 - (Y(n, 1) - theta2 * phii(M, X(n, 1))).^2)/0.00002;
         
     end
 end
